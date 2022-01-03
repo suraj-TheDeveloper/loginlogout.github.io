17871abc297545b02755d853153ec7db188b7f9a
suraj-TheDeveloper <thedevelopersuraj@gmail.com>
2022-01-03 13:06:51 +0530

    php scripts

--- phpscripts/insert.php
+++ phpscripts/insert.php
@@ -1,14 +1,23 @@
 <?php
     include("../Config/siteconfig.php");
-    try {
-        if($_POST['register']) {
-            $stmt = $connection->prepare("INSERT INTO register (name, email, phone, password, confirmpassowrd) VALUES (:name, :email, :phone, :password, :confirmpassword)");
-            $stmt->bindParam(":name", $_POST['name'], PDO::PARAM_STR);
-            $stmt->bindParam(":email", $_POST['email'], PDO::PARAM_STR);
-            $stmt->bindParam(":phone", $_POST['phone'], PDO::PARAM_STR);
-            $stmt->bindParam(":password", $_POST['password'], PDO::PARAM_STR);
-            $stmt->bindParam(":confirmpassword", $_POST['confirmpassword'], PDO::PARAM_STR);    
-        }
+    try {   
+        // print_r($_POST);
+        if($_POST['name'] == "" && $_POST['email'] == "" && $_POST['phone'] == "" && $_POST['password'] == "" && $_POST['confirmpassword'] == "") {
+            echo "Please fill all the fields";
+        } else {
+            if($_POST['password'] != $_POST['confirmpassword']) {
+                echo "Password does not match";
+            } else {
+                $stmt = $connection->prepare("INSERT INTO register (name, email, phone, password, confirmpassword) VALUES (:name, :email, :phone, :password, :confirmpassword)");
+                $stmt->bindParam(":name", $_POST['name'], PDO::PARAM_STR);
+                $stmt->bindParam(":email", $_POST['email'], PDO::PARAM_STR);
+                $stmt->bindParam(":phone", $_POST['phone'], PDO::PARAM_STR);
+                $passwordhash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $stmt->bindParam(":password", $passwordhash, PDO::PARAM_STR);
+                $stmt->bindParam(":confirmpassword", $passwordhash, PDO::PARAM_STR);   
+                $stmt->execute();
+            }
+        } 
     } catch (Exception $e) {
         echo "ERROR", $e;
     }
